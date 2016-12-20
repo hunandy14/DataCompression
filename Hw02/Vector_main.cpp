2d90356c522f7604d5ce19a04251af8242c33707
Charlotte.HonG <hunandy14@gmail.com>
2016-12-20 15:22:03 +0800

    整理代碼

--- Hw02/Vector_main.cpp
+++ Hw02/Vector_main.cpp
@@ -14,37 +14,33 @@ using namespace std;
 using namespace imr;
 
 #define AutoOpen 0
-#define Pic_name_in "IMG.raw"
-#define Pic_name_out "IMG_OUT.raw"
-#define Pic_x 256
-#define Pic_y 256
+#define IMG     "IMG.raw"
+#define IMG_OUT "IMG_OUT.raw"
 
-void creat_ori(imgraw & img);
 int main(int argc, char const *argv[]) {
     // 開圖檔
-    ImrSize size(Pic_y, Pic_x), size2(64, 64);
+    ImrSize size(256, 256), size2(64, 64);
     imgraw img(size), sou(size);
     imgraw ori(size2), tra(size2), idx(size2);
-    sou.read(Pic_name_in);
+    sou.read(IMG);
     //---------------------------------------------------------
     // 建立編碼簿
     ori.get_org(sou).write("ori.raw");
     // 建立索引
     idx.get_idx(sou, ori).write("idx.raw");
     // 輸出第一次的圖檔
-    img.merge(ori, idx).write(Pic_name_out);
+    img.merge(ori, idx).write(IMG_OUT);
     // 訓練編碼簿
-    for (int i = 0; i < 99; ++i){
+    for (int i = 0; i < 999; ++i){
         double temp = tra.tra_code(sou, idx);
         cout << "[" << setw(2) << i << "] = " << temp << endl;
-        if (temp <= 0.01){
+        if (temp <= 0.01)
             break;
-        }
     }tra.write("tra.raw");
     img.merge(tra, idx).write("IMG_tra.raw");
     //---------------------------------------------------------
     // 開啟檔案
     if(AutoOpen==1)
-        system(Pic_name_out);
+        system(IMG_OUT);
     return 0;
 }
