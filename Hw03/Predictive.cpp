73c8f6629a0609a012b6a9003d633ff766e148ce
Charlotte.HonG <hunandy14@gmail.com>
2017-01-04 13:01:32 +0800

    優化代碼

--- Hw03/Predictive.cpp
+++ Hw03/Predictive.cpp
@@ -35,6 +35,7 @@ Final: 2016/10/05
 #include "OpenRAW_fun\Operator.cpp"
 //=========================================================
 namespace imr{
+#define byte 8
 void imgraw::unscalar(imint size=4){
     imgraw img_c(ImrSize(0, 0));
     imint new_size = ceil((this->filesize*8)/size);
@@ -43,12 +44,12 @@ void imgraw::unscalar(imint size=4){
     // 旗標
     int bit_idx=0, img_idx=0;
     // bit暫存
-    bitset<8> bit;
+    bitset<byte> bit;
     // 讀壓縮檔的點(點是分區的最低值)
     for (int i = 0; i < (int)ori_size; ++i){
         // 讀每一位元
-        for (int j = 0; j < 8; ++j){
-            bit[bit_idx+(8-size)] = ((bitset<8>)((*this)[i]))[j];
+        for (int j = 0; j < byte; ++j){
+            bit[bit_idx+(byte-size)] = ((bitset<byte>)((*this)[i]))[j];
             ++bit_idx;
             // 旗標
             if(bit_idx >= (int)size) {
@@ -62,34 +63,34 @@ void imgraw::unscalar(imint size=4){
     }
     // 修復數據(取中間值)
     bit.reset();
-    for (int i = 0; i < 8-(int)size-1; ++i){
+    for (int i = 0; i < byte-(int)size-1; ++i){
         bit[i]=1;
     }
     for (int i = 0; i < (int)new_size; ++i){
         img_c[i] += (imch)bit.to_ulong();;
-        // cout << "img_c[i]=" << (bitset<8>)img_c[i] << endl;
+        // cout << "img_c[i]=" << (bitset<byte>)img_c[i] << endl;
     }
     (*this) = img_c;
 }
 
 void imgraw::scalar(imint size=4){
     imgraw img_c(ImrSize(0, 0));
-    imint new_size = ceil((this->filesize*size)/8);
+    imint new_size = ceil((this->filesize*size)/byte);
     imint ori_size = this->filesize;
     img_c.resize_canvas(new_size);
     // 旗標
     int img_idx=0, bit_idx=0;
     // bit暫存
-    bitset<8> bit;
+    bitset<byte> bit;
     // 讀取每個點
     for (int i = 0; i < (int)ori_size; ++i){
-        // cout << "(*this)[i]=" << (bitset<8>)((*this)[i]) << endl;
+        // cout << "(*this)[i]=" << (bitset<byte>)((*this)[i]) << endl;
         // 讀取位元
-        for (int j = 8-size; j < 8; ++j){
-            bit[bit_idx] = ((bitset<8>)((*this)[i]))[j];
+        for (int j = byte-size; j < byte; ++j){
+            bit[bit_idx] = ((bitset<byte>)((*this)[i]))[j];
             // 旗標
             ++bit_idx;
-            if(bit_idx==8) {
+            if(bit_idx==byte) {
                 // 輸出
                 img_c[img_idx] = (imch)bit.to_ulong();
                 ++img_idx;
